ba8d31f3c86ee330aa4f92996b64885a52ed643a
Jimmy <zhengge2012@gmail.com>
2018-07-24 16:57:48 +0800

    add django debug toolbar

--- testproject/testproject/settings.py
+++ testproject/testproject/settings.py
@@ -6,6 +6,7 @@ INSTALLED_APPS += [
     'testproject',
     'stronghold',
     'impersonate',
+    'debug_toolbar'
 ]
 
 DATABASES = {
@@ -22,6 +23,7 @@ STRONGHOLD_PUBLIC_URLS = [
 MIDDLEWARE += [
     'impersonate.middleware.ImpersonateMiddleware',
     'testproject.middleware.LoginRequiredStrongholdMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'testproject.urls'
@@ -39,3 +41,18 @@ LBWF_APPS.update({
 })
 
 TEMPLATE_DEBUG = True
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
+INTERNAL_IPS = '127.0.0.1'
